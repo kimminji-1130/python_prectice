81a2f6a3220455a2e85feb77fcc4965988b352d9
kimminji-1130 <84611998+kimminji-1130@users.noreply.github.com>
2022-06-24 22:47:14 +0900

    Create chapter_2_prctice.py

---
+++ chapter_2_prctice.py
@@ -0,0 +1,12 @@
+print('자유 은행 대출 상환금 계산 서비스에 오심을 환영합니다')
+principal = print(input('대출원금이 얼마인가요?(백만원 이상)'))
+interest = print(input('연 이자율은 몇 %인가요?'))
+year = print(input('상환기간은 몇 년인가요?'))
+
+def monthly_payment_plan(principal, interest, year):
+  pass #Write your code here.
+
+
+print('\n')
+print('대출 상환금 내역을 알려드리겠습니다.\n')
+print('대출원금: 'principald\n)
