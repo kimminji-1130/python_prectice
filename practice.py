f5efc6532a39f4b143a0e7a6c41ea98b96445759
kimminji-1130 <84611998+kimminji-1130@users.noreply.github.com>
2022-06-24 19:09:06 +0900

    Create practice.py

---
+++ practice.py
@@ -0,0 +1,33 @@
+챕터 1
+/*문자열 붙이기*/
+"Hello" + "World"
+/*빈 문자열*/
+"Hello" + "" + "World"
+
+3 + 5
+2 - 7
+24 * 365
+53/7 
+53//7
+
+/*우선순위*/
+1. ** 거듭제곱
+2. - 부호가꾸기
+3. *, /, //, % 곱하기 나누기 몫, 나머지
+4. +, - 더하기, 빼기
+
+챕터 2 변수와 함수
+/*변수 작명 규칙*/
+korea1st = 2020
+korea1st
+--2020
+python_programming = "easy"
+python_programming
+--"easy"
+/*지정문 활용 실전*/
+radius = 5
+pi = 3.14
+area = pi * radius ** 2
+printf(area)
+-- 78.5
+
